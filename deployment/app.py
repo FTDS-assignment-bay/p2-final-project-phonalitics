cbe0210e5e6141cb695723d2dcf68ac11a54189a
stanlys96 <stanlyskwok@gmail.com>
2024-11-22 10:46:46 +0700

    finalise deployment

--- deployment/app.py
+++ deployment/app.py
@@ -1,36 +1,9 @@
 import streamlit as st
-import home
-import youtube
 st.set_page_config(layout="wide")
-st.title("Phone Brands Sentiment Analysis")
 
-tab1, tab2 = st.tabs(["Word Clouds", "Youtube Comments Analysis"])
+pg = st.navigation([
+    st.Page("home.py", title="Word Clouds", icon="🔥"),
+    st.Page("youtube.py", title="Youtube Comments Analysis", icon="💬"),
+])
 
-def hide_tabbar():
-    st.sidebar.empty()
-
-with tab1:
-    st.markdown(
-        """
-        <style>
-        .css-1d391kg {
-            display: block;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    home.app()
-
-with tab2:
-    st.markdown(
-        """
-        <style>
-        .css-1d391kg {
-            display: none;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    youtube.app()
+pg.run()
--- deployment/youtube.py
+++ deployment/youtube.py
@@ -2,43 +2,44 @@ from helper import extract_youtube_id, get_all_comments
 import streamlit as st
 import random
 
-def app():
-  user_input = st.text_input("Enter a youtube link for sentiment analysis")
-  sentiment_colors = {
-      "Positive": "#28a745",  
-      "Neutral": "#ffc107",   
-      "Negative": "#dc3545"   
-  }
-  
-  if st.button('Submit', type="secondary"):
-    sentiments = ["Positive", "Neutral", "Negative"]
-    try:
-      the_youtube_id = extract_youtube_id(user_input)
-      if the_youtube_id:
-        with st.spinner("Please wait while we're loading the data..."):
-          the_data = get_all_comments(the_youtube_id)
-          st.markdown(f"""<p style="color: green; padding: 0; margin: 0;">Total comments: {len(the_data)}</p>""", unsafe_allow_html=True)
-          for data in the_data:
-            sentiment = random.choice(sentiments)
-            sentiment_color = sentiment_colors.get(sentiment, "#6c757d")
-            comment_html = f"""
-            <div style="background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 10px; padding: 20px; margin: 20px auto; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-              <p style="font-size: 18px; line-height: 1.6; color: #333; font-family: 'Arial', sans-serif;">
-                {data["comment"]}
-              </p>
-              <p style="font-size: 16px; margin-top: 15px; font-family: 'Arial', sans-serif; font-weight: bold;">
-                Sentiment Analysis: <span style="color: {sentiment_color}; font-size: 18px; font-weight: bold; padding: 5px 10px; background-color: {sentiment_color + "33"}; border-radius: 5px;">
-                  {sentiment}
-                </span>
-              </p>
-            </div>
-            """
+st.header("💬 Youtube Comments Sentiment Analysis")
 
-            st.markdown(comment_html, unsafe_allow_html=True)
-      else:
-        st.write("Invalid youtube link.")
-    except:
+user_input = st.text_input("Enter a youtube link for sentiment analysis")
+sentiment_colors = {
+    "Positive": "#28a745",  
+    "Neutral": "#ffc107",   
+    "Negative": "#dc3545"   
+}
+
+if st.button('Submit', type="secondary"):
+  sentiments = ["Positive", "Neutral", "Negative"]
+  try:
+    the_youtube_id = extract_youtube_id(user_input)
+    if the_youtube_id:
+      with st.spinner("Please wait while we're loading the data..."):
+        the_data = get_all_comments(the_youtube_id)
+        st.markdown(f"""<p style="color: green; padding: 0; margin: 0;">Total comments: {len(the_data)}</p>""", unsafe_allow_html=True)
+        for data in the_data:
+          sentiment = random.choice(sentiments)
+          sentiment_color = sentiment_colors.get(sentiment, "#6c757d")
+          comment_html = f"""
+          <div style="background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 10px; padding: 20px; margin: 20px auto; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+            <p style="font-size: 18px; line-height: 1.6; color: #333; font-family: 'Arial', sans-serif;">
+              {data["comment"]}
+            </p>
+            <p style="font-size: 16px; margin-top: 15px; font-family: 'Arial', sans-serif; font-weight: bold;">
+              Sentiment Analysis: <span style="color: {sentiment_color}; font-size: 18px; font-weight: bold; padding: 5px 10px; background-color: {sentiment_color + "33"}; border-radius: 5px;">
+                {sentiment}
+              </span>
+            </p>
+          </div>
+          """
+
+          st.markdown(comment_html, unsafe_allow_html=True)
+    else:
       st.write("Invalid youtube link.")
-  else:
-    st.write("Click the button to submit your link!")
+  except:
+    st.write("Invalid youtube link.")
+else:
+  st.write("Click the button to submit your link!")
     
