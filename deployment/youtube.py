a129d360daa20dea8ba9d4913d1b172e08e88bb3
stanlys96 <stanlyskwok@gmail.com>
2024-11-22 23:04:12 +0700

    finalize deployment

--- deployment/youtube.py
+++ deployment/youtube.py
@@ -2,6 +2,7 @@ from helper import extract_youtube_id, get_all_comments
 import streamlit as st
 import random
 import pandas as pd
+import numpy as np
 
 st.header("💬 Youtube Comments Sentiment Analysis")
 
@@ -31,20 +32,29 @@ if st.button('Submit', type="secondary"):
     if the_youtube_id:
       with st.spinner("Please wait while we're loading the data..."):
         the_data = get_all_comments(the_youtube_id)
-        print(the_data)
+        data = np.random.choice(["Positive", "Neutral", "Negative"], size=len(the_data))
+
+        # Create a DataFrame
+        sentiment_data = pd.DataFrame(data, columns=["Sentiment"])
+        sentiment_counts = sentiment_data["Sentiment"].value_counts()
+        positives = sentiment_counts.get("Positive", 0)
+        neutrals = sentiment_counts.get("Neutral", 0)
+        negatives = sentiment_counts.get("Negative", 0)
         st.balloons()
-        st.markdown(f"""<p style="color: green; padding: 0; margin: 0;">Total comments: {len(the_data)}</p>""", unsafe_allow_html=True)
-        for data in the_data:
-          sentiment = random.choice(sentiments)
-          sentiment_color = sentiment_colors.get(sentiment, "#6c757d")
+        st.markdown(f"""<p style="color: gray;">Total comments: {len(the_data)}</p>""", unsafe_allow_html=True)
+        st.markdown(f"""<p style="color: green;">Positives: {positives}</p>""", unsafe_allow_html=True)
+        st.markdown(f"""<p style="color: gray;">Neutrals: {neutrals}</p>""", unsafe_allow_html=True)
+        st.markdown(f"""<p style="color: red;">Negatives: {negatives}</p>""", unsafe_allow_html=True)
+        for index, data in enumerate(the_data):
+          sentiment_color = sentiment_colors.get(sentiment_data.iloc[index, 0], "#6c757d")
           comment_html = f"""
               <div style="background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 10px; padding: 20px; margin: 20px auto; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
                 <p style="font-size: 18px; line-height: 1.6; color: #333; font-family: 'Arial', sans-serif;">
                   {data["comment"]}
                 </p>
-                <p style="font-size: 16px; margin-top: 15px; font-family: 'Arial', sans-serif; font-weight: bold;">
+                <p style="font-size: 16px; color: gray; margin-top: 15px; font-family: 'Arial', sans-serif; font-weight: bold;">
                   Sentiment Analysis: <span style="color: {sentiment_color}; font-size: 18px; font-weight: bold; padding: 5px 10px; background-color: {sentiment_color + "33"}; border-radius: 5px;">
-                    {sentiment}
+                    {sentiment_data.iloc[index, 0]}
                   </span>
                 </p>
               </div>
